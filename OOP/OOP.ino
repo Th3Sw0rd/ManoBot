239b8e27434f28dafb5d5dcfccaad3423bc7834c
Tobias <tobiasjuhasz@gmail.com>
2019-10-29 11:26:59 -0300

    Mini

--- OOP/OOP.ino
+++ OOP/OOP.ino
@@ -4,8 +4,8 @@ bool flag = false;
 class Dedo{ 
   private:
     int pin;
-    double dPosition = 0;
     String dNombre;
+    double dPosition = 0;
     Servo servo;
   public:
     Dedo(int pin, String dNombre){
@@ -20,6 +20,7 @@ class Dedo{
       this->dPosition = map(pos, 0, 1023, 0, 180); 
       this->servo.write(this->dPosition);
     }
+    int pos;
 };
 
 Dedo Pulgar(9, "Pulgar");
@@ -27,7 +28,6 @@ Dedo Indice(10, "Indice");
 Dedo Medio(11, "Medio");
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   Pulgar.init();
   Indice.init();
@@ -36,19 +36,27 @@ void setup() {
 
 void loop() {
   if(i<1020 & flag==false){
-    i+=5;
+    i+=10;
+    Indice.pos += 1;
+    Medio.pos +=1;
   } else {
     if(i==1020){
       flag = false;
+      Indice.pos = 1020;
+      Medio.pos = 1020;
       i=1020;
     }
     if(flag){
-      i-=5;
+      i-=10;
+      Indice.pos -= 1;
+      Medio.pos -= 1;
       if(i==0){
         flag = false;
       }
     }
   }
-  Medio.moveTo(i);
-        delay(15);
+  Indice.moveTo(Indice.pos);
+  Pulgar.moveTo(i);
+  Medio.moveTo(Medio.pos);
+  delay(15);  
 }
